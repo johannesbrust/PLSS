347ba8e14f827da91ad9ef54018eb3cd4113e046
Brust <jjbrust@math139.ucsd.edu>
2022-05-10 18:38:28 -0700

    Addition of Python implementations

---
+++ PYTHON/example_3.py
@@ -0,0 +1,37 @@
+"""
+Test PLSS
+
+Projected Linear Systems Solver
+"""
+#--------------------------- example_3.py -------------------------------------
+#
+# Test of the Python 3 version of PLSS
+#
+# This problem is a bidiagonal matrix from experiments using 
+# CRAIG's method from Standford SOL
+# 
+# The solution with n=10 is [n,n-1,...,1]
+#------------------------------------------------------------------------------
+# J.B., 05/10/22, Initial version
+
+# Import
+import numpy as np
+import plss_R as pl
+
+# Test uses a matrix from SOL (Systems Optimization Laboratory)
+# in testing CRAIG's method
+
+n = 10;
+m = n;
+
+# Form bidiagonal matrix and solution
+A       = np.diag(np.arange(1., 1.+n)) + np.diag(np.arange(1., n),-1);
+xsol    = np.array(np.arange(n,0,-1.));
+
+b = A @ xsol
+
+# Call PLSS
+# All default values are used, except results are printed
+x0 = np.zeros(np.size(xsol))
+
+(xk,rk,outs) = pl.plss_R(x0,A,b,printF=1)
---
+++ PYTHON/example_4.py
@@ -0,0 +1,40 @@
+"""
+Test PLSS
+
+Projected Linear Systems Solver
+"""
+#--------------------------- example_4.py -------------------------------------
+#
+# Test of the Python 3 version of PLSS W
+#
+# This problem is based on transposing the bidiagonal matrix from 
+# experiments using CRAIG's method from Standford SOL
+# 
+# The solution with n=10 is [n,n-1,...,1]
+#------------------------------------------------------------------------------
+# J.B., 05/10/22, Initial version
+
+# Import
+import numpy as np
+import plss_RW2 as plw2
+import plss_R as pl
+
+# Test uses a matrix from SOL (Systems Optimization Laboratory)
+# in testing CRAIG's method
+
+n = 10;
+m = n;
+
+# Form bidiagonal matrix and solution
+A       = np.diag(np.arange(1., 1.+n)) + np.diag(np.arange(1., n),-1);
+xsol    = np.array(np.arange(n,0,-1.));
+
+b = A.T @ xsol
+
+# Call PLSS
+# All default values are used, except results are printed
+x0 = np.zeros(np.size(xsol))
+
+(xk,rk,outs) = plw2.plss_RW2(x0,A.T,b,printF=1,useW=1)
+
+#(xk,rk,outs) = pl.plss_R(x0,A.T,b,printF=1)
